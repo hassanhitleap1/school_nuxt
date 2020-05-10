1a1bed3d39accb2d8f0fc39fe2b792f4d5ebddec
hassanhitleap1 <hassanhitleap1@gmail.com>
2020-05-10 14:52:35 -0700

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,15 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+ 
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' }
+    ],
+    script: [
+      { type: 'javascript/text', src: 'https://code.jquery.com/jquery-3.4.1.slim.min.js' },
+      { type: 'javascript/text', src: 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' },
+      { type: 'javascript/text', src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' }
     ]
   },
   /*
@@ -22,8 +29,11 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: ['@/assets/css/main.css'],
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
+  },
   /*
   ** Plugins to load before mounting the App
   */
